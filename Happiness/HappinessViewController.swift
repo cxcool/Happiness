02728aee1a2968fbf69263e5c8ef4f65a20a9255
cai <307241868@qq.com>
2015-07-12 17:27:53 +0800

    SmileFace
    UIBezierPath

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by 菜 on 15/7/11.
+//  Copyright © 2015年 菜. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+}
